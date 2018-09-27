75972d10196cdcc92d108ce14de93dd3252a3fa3
yangfan2016 <15234408101@163.com>
2018-09-27 11:04:22 +0800

    clean nouse files

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
     mode:"production",
     entry: {
-        "yan":path.resolve(__dirname, '../src/yan.js')
+        "yan":path.resolve(__dirname, './src/index.js')
     },
     output: {
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, './dist'),
         filename: '[name].min.js',
         publicPath: "./dist/",
         libraryTarget: 'umd'
